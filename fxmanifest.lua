43591b414d8c8383756ea2f69ee0b1879f6b8289
Redven-Biker <marween35620@gmail.com>
2021-03-25 11:18:35 +0100

    Change to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Redven Biker (XLife.fr)'
+description 'Discord : https://discord.gg/C3aN2bBVbB'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    'index.html',
+    'css/style.css',
+    'img/background.jpg',
+    'music/music.mp3'
+}
